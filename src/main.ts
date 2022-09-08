f8b045967d893ec6f0a48c8dd4671029a7ebbe2e
Sebastian Tigges <g-threepwood@gmx.de>
2022-09-08 16:52:10 +0000

    ...

--- src/main.ts
+++ src/main.ts
@@ -150,7 +150,7 @@ class Reolink810a extends utils.Adapter {
 if (require.main !== module) {
 	// Export the constructor in compact mode
 	// module.exports = (options: Partial<utils.AdapterOptions> | undefined) => new Reolink810a(options);
-    module.exports = (options) => new Reolink810a(options);
+    module.exports = (options : any) => new Reolink810a(options);
 } else {
 	// otherwise start the instance directly
 	(() => new Reolink810a())();
