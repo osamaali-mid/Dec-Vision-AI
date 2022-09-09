7b07a27096ac92b3a71d82c50bf20a4a1904d745
Sebastian Tigges <g-threepwood@gmx.de>
2022-09-09 18:58:56 +0200

    First Alpha Release

--- build/main.js
+++ build/main.js
@@ -35,6 +35,8 @@ class Reolink810a extends utils.Adapter {
   }
   async onReady() {
     this.announceOffline();
+    this.announceOnline();
+    return;
     if (!this.config.Hostname) {
       this.log.error("Hostname not (yet) set - please check Settings!");
       return;
